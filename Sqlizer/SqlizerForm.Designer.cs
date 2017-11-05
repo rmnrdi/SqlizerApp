81878eac38e7359f2b47460eb6cec5bf6b170b73
Rob Minardi <rmnrdi@gmail.com>
2017-11-05 07:55:37 -0600

    Initial Project

---
+++ Sqlizer/SqlizerForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Sqlizer
+{
+    partial class SqlizerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SqlizerForm));
+            this.txtSql = new System.Windows.Forms.TextBox();
+            this.btnStringify = new System.Windows.Forms.Button();
+            this.btnSqlize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearSqlText = new System.Windows.Forms.Button();
+            this.btnClearStringText = new System.Windows.Forms.Button();
+            this.pbxUpArrow = new System.Windows.Forms.PictureBox();
+            this.pbxDownArrow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUpArrow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDownArrow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSql
+            // 
+            this.txtSql.Location = new System.Drawing.Point(18, 38);
+            this.txtSql.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSql.Multiline = true;
+            this.txtSql.Name = "txtSql";
+            this.txtSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSql.Size = new System.Drawing.Size(1519, 376);
+            this.txtSql.TabIndex = 0;
+            // 
+            // btnStringify
+            // 
+            this.btnStringify.Location = new System.Drawing.Point(18, 426);
+            this.btnStringify.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStringify.Name = "btnStringify";
+            this.btnStringify.Size = new System.Drawing.Size(230, 75);
+            this.btnStringify.TabIndex = 1;
+            this.btnStringify.Text = "Stringify";
+            this.btnStringify.UseVisualStyleBackColor = true;
+            this.btnStringify.Click += new System.EventHandler(this.btnStringify_Click);
+            // 
+            // btnSqlize
+            // 
+            this.btnSqlize.Location = new System.Drawing.Point(919, 514);
+            this.btnSqlize.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSqlize.Name = "btnSqlize";
+            this.btnSqlize.Size = new System.Drawing.Size(230, 75);
+            this.btnSqlize.TabIndex = 1;
+            this.btnSqlize.Text = "Sqlize";
+            this.btnSqlize.UseVisualStyleBackColor = true;
+            this.btnSqlize.Click += new System.EventHandler(this.btnSqlize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Put SQL Here";
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(23, 594);
+            this.txtString.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtString.Multiline = true;
+            this.txtString.Name = "txtString";
+            this.txtString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtString.Size = new System.Drawing.Size(1519, 273);
+            this.txtString.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 564);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Put String Here";
+            // 
+            // btnClearSqlText
+            // 
+            this.btnClearSqlText.Location = new System.Drawing.Point(266, 426);
+            this.btnClearSqlText.Name = "btnClearSqlText";
+            this.btnClearSqlText.Size = new System.Drawing.Size(230, 75);
+            this.btnClearSqlText.TabIndex = 3;
+            this.btnClearSqlText.Text = "Clear ";
+            this.btnClearSqlText.UseVisualStyleBackColor = true;
+            this.btnClearSqlText.Click += new System.EventHandler(this.btnClearSqlText_Click);
+            // 
+            // btnClearStringText
+            // 
+            this.btnClearStringText.Location = new System.Drawing.Point(1166, 514);
+            this.btnClearStringText.Name = "btnClearStringText";
+            this.btnClearStringText.Size = new System.Drawing.Size(230, 75);
+            this.btnClearStringText.TabIndex = 4;
+            this.btnClearStringText.Text = "Clear";
+            this.btnClearStringText.UseVisualStyleBackColor = true;
+            this.btnClearStringText.Click += new System.EventHandler(this.btnClearStringText_Click);
+            // 
+            // pbxUpArrow
+            // 
+            this.pbxUpArrow.Image = ((System.Drawing.Image)(resources.GetObject("pbxUpArrow.Image")));
+            this.pbxUpArrow.Location = new System.Drawing.Point(711, 422);
+            this.pbxUpArrow.Name = "pbxUpArrow";
+            this.pbxUpArrow.Size = new System.Drawing.Size(201, 167);
+            this.pbxUpArrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxUpArrow.TabIndex = 5;
+            this.pbxUpArrow.TabStop = false;
+            // 
+            // pbxDownArrow
+            // 
+            this.pbxDownArrow.Image = global::Sqlizer.Properties.Resources.Download_grey_inverse_256x;
+            this.pbxDownArrow.Location = new System.Drawing.Point(502, 426);
+            this.pbxDownArrow.Name = "pbxDownArrow";
+            this.pbxDownArrow.Size = new System.Drawing.Size(186, 163);
+            this.pbxDownArrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxDownArrow.TabIndex = 6;
+            this.pbxDownArrow.TabStop = false;
+            // 
+            // SqlizerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1550, 881);
+            this.Controls.Add(this.pbxDownArrow);
+            this.Controls.Add(this.pbxUpArrow);
+            this.Controls.Add(this.btnClearStringText);
+            this.Controls.Add(this.btnClearSqlText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSqlize);
+            this.Controls.Add(this.btnStringify);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.txtSql);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "SqlizerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sqlizer";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUpArrow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDownArrow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSql;
+        private System.Windows.Forms.Button btnStringify;
+        private System.Windows.Forms.Button btnSqlize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearSqlText;
+        private System.Windows.Forms.Button btnClearStringText;
+        private System.Windows.Forms.PictureBox pbxUpArrow;
+        private System.Windows.Forms.PictureBox pbxDownArrow;
+    }
+}
+
---
+++ Sqlizer/SqlizerForm.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sqlizer
+{
+    public partial class SqlizerForm : Form
+    {
+        public SqlizerForm()
+        {
+            InitializeComponent();
+
+  //          string sqlString = @"'SELECT A1.*, StatusItem.Name as StatusName FROM ' +
+  //            '(SELECT RxNum, Account, RSphere, LSphere, RCyl, LCyl, RAdd, LAdd, Patient, RxDate, RxTime, ShipDate, ShipTime, OperatorID, TermCode FROM RxArchive ' +
+  //            'WHERE Termcode > 3 AND Termcode <> 7 AND ShipDate BETWEEN ''' + DateTimeToPVStr(startDate) + ''' AND ''' + DateTimeToPVStr(endDate) + ''') AS A1 ' +
+  //            'INNER JOIN StatusItem ON A1.TermCode = StatusItem.Num WHERE StatusItem.SeqNum2 & 1 = 0 ORDER BY Patient, RxNum;';";
+
+  //          string rawSql = @"SELECT A1.*, StatusItem.Name as StatusName FROM  
+  //            (SELECT RxNum, Account, RSphere, LSphere, RCyl, LCyl, RAdd, LAdd, Patient, RxDate, RxTime, ShipDate, ShipTime, OperatorID, TermCode FROM RxArchive  
+  //            WHERE Termcode > 3 AND Termcode <> 7 AND ShipDate BETWEEN   '**INSERT DATE VALUE HERE**'  AND   '**INSERT DATE VALUE HERE**' ) AS A1  
+  //            INNER JOIN StatusItem ON A1.TermCode = StatusItem.Num WHERE StatusItem.SeqNum2 & 1 = 0 ORDER BY Patient, RxNum; ";
+
+  //          string builtSqlString = @"  BuiltSQL := 'SELECT FrameAttr.AttrName as Collection, AllSaveCollection.* FROM FrameAttr '; 
+  //BuiltSQL := BuiltSQL + 'RIGHT OUTER JOIN '; 
+  //BuiltSQL := BuiltSQL + '(SELECT MfrName, StyleName, ColorName, SKU, Eye, Bridge, Temple, Price, Cost, (Cost * OnHand) AS Valuation, Bin, OnHand, OnOrder, Flags, CollID FROM';
+  //BuiltSQL := BuiltSQL + '(SELECT ItemsMfrStyle.*, FrameColor.Name as ColorName FROM ';
+  //BuiltSQL := BuiltSQL + '(SELECT ItemsMFR.*, FrameStyle.StyleName, FrameStyle.UType as CollID FROM ';
+  //BuiltSQL := BuiltSQL + '(SELECT FrameMFR.Name as MfrName, Sku, Temple, Bridge, Eye, Color as ColorNum, FrameItem as StyleNum, ManufacturerNumber as MfrNum, Price1 as Cost, Cost as Price, Bin, OnHand, OnOrder, Flags '; 
+  //BuiltSQL := BuiltSQL + 'FROM FrameItem ';
+  //{Handle frame item flags}
+  //BuiltSQL := BuiltSQL + 'INNER JOIN FrameMFR ON MfrNum = FrameMFR.Num WHERE ';
+  //BuiltSQL := BuiltSQL + 'flags & ' + IntToStr(flagInt) + ' = 0 AND (';
+  //if Manufacturers <> '' then begin
+  //  BuiltSQL := BuiltSQL + '(Name = ''' + ChopOff(Manufacturers, ';') + ''') ';
+  //  while Manufacturers <> '' do begin
+  //    BuiltSQL := BuiltSQL + 'OR (Name = ''' + ChopOff(Manufacturers, ';') + ''') ';
+  //  end;
+  //end;
+  //BuiltSQL := BuiltSQL + ')) AS ItemsMFR ';
+  //BuiltSQL := BuiltSQL + 'INNER JOIN FrameStyle ON ItemsMFR.MfrNum = FrameStyle.MfrNum AND ItemsMFR.StyleNum = FrameStyle.StyleNum) AS ItemsMFRStyle ';
+  //BuiltSQL := BuiltSQL + 'INNER JOIN FrameColor ON ItemsMFRStyle.ColorNum = FrameColor.ColorNum AND ItemsMFRStyle.StyleNum = FrameColor.ColorName AND ItemsMFRStyle.MfrNum = FrameColor.MfrNum) AS Frames ';
+  //BuiltSQL := BuiltSQL + 'GROUP BY MfrName, StyleName, ColorName, SKU, Eye, Bridge, Temple, Bin, Price, Cost, OnHand, OnOrder, Flags, CollID) AS AllSaveCollection ';
+  //BuiltSQL := BuiltSQL + 'ON FrameAttr.AttrID = CollID ';
+  //BuiltSQL := BuiltSQL + 'ORDER BY MfrName, StyleName, ColorName, SKU';";
+
+            //txtSql.Text = rawSql;
+
+            //txtSql.Text = rawSql;
+        }
+
+        /// <summary>
+        /// Converts SQL into string
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStringify_Click(object sender, EventArgs e)
+        {
+            var sqlInput = txtSql.Text;
+
+            string[] stringAsArray = sqlInput.Split(' ');
+
+
+            string dateFunctionAdded = "";
+            foreach (string item in stringAsArray)
+            {
+                if (item.Contains("---") && !String.IsNullOrEmpty(item))
+                {
+                    dateFunctionAdded += item.Replace(item, "''' + DateTimeToPVStr(---startDate/endDate---) + '''");
+                }
+                else if (item.Contains("BuiltSQL") || item.Contains(":="))
+                {
+                    dateFunctionAdded += item.Replace(item, "");
+                }
+                else
+                {
+                    dateFunctionAdded += item + " ";
+                }
+            }
+
+            //string[] withDateAdded = dateFunctionAdded.Split(' ');
+
+            var builtSqlStringAdded = "";
+            int iterator = 0;
+            foreach (var line in dateFunctionAdded.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (iterator == 0)
+                {
+                    builtSqlStringAdded += "BuiltSQL := ' " + line.Trim() + "';" + Environment.NewLine;
+                    iterator++;
+                }
+                else if (iterator > 0)
+                {
+                    builtSqlStringAdded += "BuiltSQL := BuiltSQL + '" + line.Trim() + "';" + Environment.NewLine;
+                }
+            }
+
+            var finishedString = builtSqlStringAdded;
+
+            ClearBothTextBoxes();
+
+            txtString.Text = finishedString;
+        }
+
+        /// <summary>
+        /// Converts a string into SQL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSqlize_Click(object sender, EventArgs e)
+        {
+            string stringInput = txtString.Text;
+
+            string plusesRemoved = RemoveChars(stringInput, '+', '\'');
+
+            string singleQuotesRemoved = string.Join(" ", plusesRemoved.Split(' ').Select(x => x.Trim('\'')));
+
+            string preStripped = singleQuotesRemoved.Trim('\'', ';');
+
+            string fullyStripped = preStripped.Replace(";", "");
+
+            string semicolonAdded = fullyStripped += ';';
+
+            string[] stringAsArray = semicolonAdded.Split(' ');
+
+
+            string dateFunctionRemoved = "";
+            foreach (string item in stringAsArray)
+            {
+                if (item.Contains("Date") && item.Contains("(") && !String.IsNullOrEmpty(item))
+                {
+                    dateFunctionRemoved += item.Replace(item, "'---Insert-Date-Here---'");
+                }
+                else if (item.Contains("BuiltSQL") || item.Contains(":="))
+                {
+                    dateFunctionRemoved += item.Replace(item, "");
+                }
+                else
+                {
+                    dateFunctionRemoved += item + " ";
+                }
+            }
+
+            string finalSqlString = dateFunctionRemoved;
+            ClearBothTextBoxes();
+
+            txtSql.Text = finalSqlString;
+        }
+        public void ClearBothTextBoxes()
+        {
+            txtSql.Clear();
+            txtString.Clear();
+        }
+
+        public string RemoveChars(string input, params char[] chars)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!chars.Contains(input[i]))
+                    sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+
+        private void btnClearSqlText_Click(object sender, EventArgs e)
+        {
+            txtSql.Clear();
+        }
+
+        private void btnClearStringText_Click(object sender, EventArgs e)
+        {
+            txtString.Clear();
+        }
+    }
+}
